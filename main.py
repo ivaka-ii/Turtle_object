9b0dfea7bad2ebb4660814c0d2ec0fa000e27042
Ivaylo Ivanov <mr.ivanov.iv@gmail.com>
2020-08-04 09:03:56 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,10 @@
+import turtle
+
+ivo_turtle = turtle.Turtle()
+ivo_turtle.forward(100)
+ivo_turtle.right(90)
+ivo_turtle.forward(100)
+ivo_turtle.right(90)
+ivo_turtle.forward(100)
+ivo_turtle.right(90)
+ivo_turtle.forward(100)
