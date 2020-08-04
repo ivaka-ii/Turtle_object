9fe424dd0c77ccc1b075d87d1af0ad2797015433
Ivaylo Ivanov <mr.ivanov.iv@gmail.com>
2020-08-04 09:15:16 +0000

    Made function to turtle

--- main.py
+++ main.py
@@ -1,10 +1,17 @@
 import turtle
 
+# This is a square
 ivo_turtle = turtle.Turtle()
-ivo_turtle.forward(100)
-ivo_turtle.right(90)
-ivo_turtle.forward(100)
-ivo_turtle.right(90)
-ivo_turtle.forward(100)
-ivo_turtle.right(90)
-ivo_turtle.forward(100)
+
+def square():
+  ivo_turtle.forward(100)
+  ivo_turtle.right(90)
+  ivo_turtle.forward(100)
+  ivo_turtle.right(90)
+  ivo_turtle.forward(100)
+  ivo_turtle.right(90)
+  ivo_turtle.forward(100)
+
+square()
+ivo_turtle.forward(200)
+square()
